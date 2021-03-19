0390fab58d01cfe72f1124619eb2cea61c7ec726
EvgeniErmakov <evgeni_ermakov@mail.ru>
2021-03-19 20:12:09 +0300

    Update SolutionTwo.java

--- SolutionTwo.java
+++ SolutionTwo.java
@@ -1,6 +1,6 @@
 public class SolutionTwo {
     public static void main(String[] args) {
         System.out.println("Java: version 11.");
-        System.out.println("JVM: HotSpot");
+        System.out.println("JVM: HotSpot.");
     }
 }
